1135fff1f5511cd1a8df0a3df9ec795f17a68b25
Ben Bruning <bruning@gmail.com>
2018-09-06 19:49:49 -0400

    linting

--- react.js
+++ react.js
@@ -71,8 +71,8 @@ module.exports = {
 
     // TODO: Over time try to remove these - Airbnb suggestions
     "max-len": [0, { "code": 400 }], // try to bring this down to at least 120 over time
-    //"no-else-return": 0,
-    //"lines-between-class-members": 0,
+    "no-else-return": 0,
+    "lines-between-class-members": 0,
     "react/no-access-state-in-setstate": 0,
     "react/sort-comp": 0,
     "react/require-default-props": 0,
