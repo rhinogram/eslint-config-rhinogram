26ef2835dde2a184ff4ae72fa16ecdbb5671ac2a
Ben Bruning <bruning@gmail.com>
2018-10-11 12:49:03 -0400

    rule update

--- node.js
+++ node.js
@@ -1,17 +1,16 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "es6": true
+  env: {
+    node: true,
+    es6: true
   },
-  "extends": "airbnb-base",
-  "plugins": [
-    "babel"
-  ],
-  "rules": {
+  extends: "airbnb-base",
+  plugins: ["babel"],
+  rules: {
     "no-unused-vars": 1,
     "arrow-body-style": 1,
     "arrow-parens": 1,
     "import/prefer-default-export": 0,
-    "max-len": [1, { "code": 170 }],
+    "max-len": [1, { code: 170 }],
+    "no-use-before-define": 0
   }
 };
