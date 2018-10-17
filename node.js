4b963664aeaf5e952c897d5dbcb9fa27d5e990c3
Grady Rogers <rogersgt819@gmail.com>
2018-10-17 12:56:28 -0400

    allow es6 arrow functions and do not enforce spread operator

--- node.js
+++ node.js
@@ -7,12 +7,13 @@ module.exports = {
   plugins: ["babel"],
   rules: {
     "no-unused-vars": 1,
-    "arrow-body-style": 1,
-    "arrow-parens": 1,
+    "arrow-body-style": [2, "as-needed"],
+    "arrow-parens": 0,
     "import/prefer-default-export": 0,
     "max-len": [1, { code: 170 }],
     "no-use-before-define": 0,
     "no-param-reassign": 0,
+    "prefer-spread": 0,
     "import/no-cycle": 0
   }
 };
