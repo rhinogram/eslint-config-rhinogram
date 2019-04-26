1a791064a375fde2856baebcc4cdf1cc90556633
Ben Bruning <bruning@gmail.com>
2019-04-26 14:16:32 -0400

    updating rules

--- node.js
+++ node.js
@@ -1,24 +1,25 @@
 module.exports = {
   env: {
     node: true,
-    es6: true
+    es6: true,
+    'jest/globals': true,
   },
-  extends: "airbnb-base",
-  plugins: ["babel"],
+  extends: 'airbnb-base',
+  plugins: ['babel', 'jest'],
   rules: {
-    "no-unused-vars": 1,
-    "arrow-body-style": 1,
-    "arrow-parens": 1,
-    "import/prefer-default-export": 0,
-    "max-len": ['error', 170, 2, {
+    'no-unused-vars': 1,
+    'arrow-body-style': 1,
+    'arrow-parens': 1,
+    'import/prefer-default-export': 0,
+    'max-len': ['error', 170, 2, {
       ignoreUrls: true,
       ignoreComments: false,
       ignoreRegExpLiterals: true,
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
     }],
-    "no-use-before-define": 0,
-    "no-param-reassign": 0,
-    "import/no-cycle": 0
-  }
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+    'import/no-cycle': 0,
+  },
 };
